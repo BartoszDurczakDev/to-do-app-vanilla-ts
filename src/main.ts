2072026c2a8b6852f588b9e43ab308890645ddcb
Bartosz Durczak <bartoszdurczak.dev@gmail.com>
2025-09-14 22:28:08 +0200

    fix: missing router and style registration added into main file

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
+import { Router } from './core/routing/router';
+import { routes } from './core/routing/routes';
+import './style.css';
+
 const mainApp = document.querySelector<HTMLDivElement>('#app')!;
+
+export const router = new Router(mainApp, routes);
